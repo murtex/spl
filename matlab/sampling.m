cbd046164038fb06b5dd072cf615f379d4b4f0f2
murtex <kuberski@uni-potsdam.de>
2016-01-19 01:54:40 +0100

    updated dsp sampling

--- matlab/sampling.m
+++ matlab/sampling.m
@@ -1,43 +1,49 @@
 clearvars( '-except', 'fig_ad', 'fig_da' );
 
-	% prepare continuous sine signal
-L = 1; % length in seconds
+	% -----------------------------------------------------------------------
+	% set up a continuous test signal (sine with frequency f and length L)
+	% -----------------------------------------------------------------------
+f = 1;
+L = 1;
 
-f = 1; % sine frequency
-dt = 1/1000; % temporal resolution
-
-t = linspace( 0, L, L/dt ); % "continuous" signal, TEST!
-xt = sin( 2*pi*f * t );
-
-	% quantize continuous signal
-fS = 48; % sampling rate, TEST!
-nS = 3; % bits per sample, TEST!
-
-N = floor( L * fS ); % number of samples
-i = (1:N); % sample indices
-
-ti = (i - 1) / fS; % discrete signal
-xi = round( 2^(nS-1) * sin( 2*pi*f * ti ) ) / 2^(nS-1);
+x = @( t ) sin( 2*pi*f * t ); % continuous sine with frequency f
 
 	% -----------------------------------------------------------------------
-	% the next lines are not important for following the lecture!
+	% quantize the test signal (using sampling rate fS and nS bits/sample)
 	% -----------------------------------------------------------------------
-	
-	% reconstruct signal (whittaker-shannon interpolation)
+fS = 48; % sampling rate, EXERCISE!
+nS = 3; % bits per sample, EXERCISE!
+
+N = floor( L * fS );
+ti = (0:N-1) / fS; % quantized time values
+xi = round( 2^(nS-1) * x( ti ) ) / 2^(nS-1); % quantized amplitudes
+
+	% -----------------------------------------------------------------------
+	% reconstruct the signal from qunatization (Whittaker-Shannon)
+	% THIS PART IS NOT IMPORTANT FOR FOLLOWING THE LECTURE!
+	% -----------------------------------------------------------------------
+dt = 1 / 2000; % temporal resolution
+t = linspace( 0, L, L / dt );
+
 for j = 1:numel( t )
-	sa = pi * (t(j) - (i-1) / fS) * fS; % sinc function
-	sa(find( sa == 0)) = 1;
-	h = sin( sa ) ./ sa;
-	xr(j) = sum( xi .* h ); % reconstructed amplitude
+	sincarg = (t(j) - ti) * fS; % sinc function
+	sincarg(find( sincarg == 0)) = 1;
+	sincval = sin( pi * sincarg ) ./ (pi * sincarg);
+
+	xr(j) = sum( xi .* sincval ); % reconstructed amplitude
 end
 
 fR = sum( abs( diff( xr >= 0 ) ) ) / L / 2; % estimate frequency using zero-crossings, TODO
-	
-	% plot quantization
+
+	% -----------------------------------------------------------------------
+	% plot A/D conversion figure
+	% THIS PART IS NOT IMPORTANT FOR FOLLOWING THE LECTURE!
+	% -----------------------------------------------------------------------
 if exist( 'fig_ad', 'var' ) ~= 1 || ~ishandle( fig_ad ) % prepare figure window
 	fig_ad = figure( ...
-		'Name', 'QUANTIZATION', ...
+		'Name', 'A/D CONVERSION', ...
 		'Color', [0.9, 0.9, 0.9], 'InvertHardcopy', 'off', ...
+		'PaperPosition', [0, 0, 8, 6], ...
 		'defaultAxesFontName', 'DejaVu Sans Mono', 'defaultAxesFontSize', 20, 'defaultAxesFontWeight', 'bold', ...
 		'defaultAxesNextPlot', 'add', ...
 		'defaultAxesBox', 'on', 'defaultAxesLayer', 'top', ...
@@ -47,15 +53,15 @@ end
 figure( fig_ad ); % set and clear current figure
 clf( fig_ad );
 
-title( 'QUANTIZATION' );
+title( 'A/D CONVERSION' ); % set labels
 
 xlabel( 'time in seconds' );
 ylabel( 'amplitude' );
 
 xlim( [0, L] ); % set axes
-ylim( [-1, 1] * max( abs( cat( 2, xt, xi, xr ) ) ) * 1.1 );
+ylim( [-1, 1] * max( abs( cat( 2, x( t ), xi, xr ) ) ) * 1.1 );
 
-plot( t, xt, ... % plot continuous signal
+plot( t, x( t ), ... % plot continuous signal
 	'Color', 'blue', 'LineWidth', 2 );
 
 stem( ti, xi, ... % plot discrete signal
@@ -65,12 +71,18 @@ stem( ti, xi, ... % plot discrete signal
 legend( ... % show legend
 	{sprintf( 'continuous sine (%.1fHz)', f ), sprintf( 'quantization (%.1fHz, %dbit)', fS, nS )}, ...
 	'Location', 'southeast' );
+	
+print( fig_ad, 'sampling_ad', '-depsc2' ); % write figure
 
-	% plot reconstruction
+	% -----------------------------------------------------------------------
+	% plot D/A conversion figure
+	% THIS PART IS NOT IMPORTANT FOR FOLLOWING THE LECTURE!
+	% -----------------------------------------------------------------------
 if exist( 'fig_da', 'var' ) ~= 1 || ~ishandle( fig_da ) % prepare figure window
 	fig_da = figure( ...
-		'Name', 'RECONSTRUCTION', ...
+		'Name', 'D/A CONVERSION', ...
 		'Color', [0.9, 0.9, 0.9], 'InvertHardcopy', 'off', ...
+		'PaperPosition', [0, 0, 8, 6], ...
 		'defaultAxesFontName', 'DejaVu Sans Mono', 'defaultAxesFontSize', 20, 'defaultAxesFontWeight', 'bold', ...
 		'defaultAxesNextPlot', 'add', ...
 		'defaultAxesBox', 'on', 'defaultAxesLayer', 'top', ...
@@ -80,13 +92,13 @@ end
 figure( fig_da ); % set and clear current figure
 clf( fig_da );
 
-title( 'RECONSTRUCTION' );
+title( 'D/A CONVERSION' ); % set labels
 
 xlabel( 'time in seconds' );
 ylabel( 'amplitude' );
 
 xlim( [0, L] ); % set axes
-ylim( [-1, 1] * max( abs( cat( 2, xt, xi, xr ) ) ) * 1.1 );
+ylim( [-1, 1] * max( abs( cat( 2, x( t ), xi, xr ) ) ) * 1.1 );
 
 stem( ti, xi, ... % plot discrete signal
 	'Color', 'red', 'LineWidth', 2, 'MarkerSize', 4, 'MarkerFaceColor', 'red', ...
@@ -99,7 +111,5 @@ legend( ... % show legend
 	{'quantized signal', sprintf( 'reconstruction (~%.1fHz)', fR )}, ...
 	'Location', 'southeast' );
 
-	% write image
-print( fig_ad, 'sampling_ad', '-depsc2' );
-print( fig_da, 'sampling_da', '-depsc2' );
+print( fig_da, 'sampling_da', '-depsc2' ); % write figure
 
